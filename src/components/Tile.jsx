681b337434953c61b5dbb498b37922972d983ca0
abhijit181 <abhijit.patil@cctech.co.in>
2024-05-11 10:18:41 +0530

    country class added

--- src/components/Tile.jsx
+++ src/components/Tile.jsx
@@ -1,15 +1,15 @@
-import styles from "./tile.module.css";
+import "./tile.css";
 
 const Tile = ({ imgUrl, countryName, alt }) => {
   return (
     <>
-      <div className={styles.countryCard}>
+      <div className="countryCard">
         <img
           src={imgUrl}
           alt={alt}
           style={{ width: "100px", height: "100px" }}
         />
-        <p style={{ fontSize: "1.2rem" }}>{countryName}</p>
+        <p style={{ fontSize: "1.2rem", textAlign: "center" }}>{countryName}</p>
       </div>
     </>
   );
